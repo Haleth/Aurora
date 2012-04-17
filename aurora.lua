38486c0b745c5c521eb944548d1cfe8b68e8f9dc
Haleth <haleth@live.com>
2012-04-18 01:58:17 +0200

    Reskin the achievement comparison window.

--- aurora.lua
+++ aurora.lua
@@ -2776,6 +2776,14 @@ Skin:SetScript("OnEvent", function(self, event, addon)
 		AchievementFrameSummaryAchievementsHeaderHeader:Hide()
 		AchievementFrameSummaryCategoriesHeaderTexture:Hide()
 		select(3, AchievementFrameStats:GetChildren()):Hide()
+		select(5, AchievementFrameComparison:GetChildren()):Hide()
+		AchievementFrameComparisonHeaderBG:Hide()
+		AchievementFrameComparisonHeaderPortrait:Hide()
+		AchievementFrameComparisonHeaderPortraitBg:Hide()
+		AchievementFrameComparisonBackground:Hide()
+		AchievementFrameComparisonDark:SetAlpha(0)
+		AchievementFrameComparisonSummaryPlayerBackground:Hide()
+		AchievementFrameComparisonSummaryFriendBackground:Hide()
 
 		local first = 1
 		hooksecurefunc("AchievementFrameCategories_Update", function()
@@ -2793,15 +2801,6 @@ Skin:SetScript("OnEvent", function(self, event, addon)
 		AchievementFrameFilterDropDownText:SetPoint("CENTER", -10, 1)
 
 		AchievementFrameSummaryCategoriesStatusBar:SetStatusBarTexture(C.media.backdrop)
-
-		for i = 1, 3 do
-			local tab = _G["AchievementFrameTab"..i]
-			if tab then
-				F.CreateTab(tab)
-			end
-		end
-
-		AchievementFrameSummaryCategoriesStatusBar:SetStatusBarTexture(C.media.backdrop)
 		AchievementFrameSummaryCategoriesStatusBar:GetStatusBarTexture():SetGradient("VERTICAL", 0, .4, 0, 0, .6, 0)
 		AchievementFrameSummaryCategoriesStatusBarLeft:Hide()
 		AchievementFrameSummaryCategoriesStatusBarMiddle:Hide()
@@ -2816,6 +2815,13 @@ Skin:SetScript("OnEvent", function(self, event, addon)
 		bg:SetPoint("BOTTOMRIGHT", 1, -1)
 		bg:SetFrameLevel(AchievementFrameSummaryCategoriesStatusBar:GetFrameLevel()-1)
 		F.CreateBD(bg, .25)
+
+		for i = 1, 3 do
+			local tab = _G["AchievementFrameTab"..i]
+			if tab then
+				F.CreateTab(tab)
+			end
+		end
 
 		for i = 1, 7 do
 			local bu = _G["AchievementFrameAchievementsContainerButton"..i]
@@ -2870,7 +2876,7 @@ Skin:SetScript("OnEvent", function(self, event, addon)
 		hooksecurefunc("AchievementButton_GetProgressBar", function(index)
 			local bar = _G["AchievementFrameProgressBar"..index]
 			if not bar.reskinned then
-				bar:SetStatusBarTexture(C.media.backdrop)
+				bar:SetStatusBarTexture(C.media.texture)
 
 				_G["AchievementFrameProgressBar"..index.."BG"]:SetTexture(0, 0, 0, .25)
 				_G["AchievementFrameProgressBar"..index.."BorderLeft"]:Hide()
@@ -2937,7 +2943,7 @@ Skin:SetScript("OnEvent", function(self, event, addon)
 			bg:SetPoint("BOTTOMRIGHT", 1, -1)
 			bg:SetFrameLevel(bu:GetFrameLevel()-1)
 			F.CreateBD(bg, .25)
-			
+
 			_G["AchievementFrameSummaryCategoriesCategory"..i.."Left"]:Hide()
 			_G["AchievementFrameSummaryCategoriesCategory"..i.."Middle"]:Hide()
 			_G["AchievementFrameSummaryCategoriesCategory"..i.."Right"]:Hide()
@@ -2953,11 +2959,93 @@ Skin:SetScript("OnEvent", function(self, event, addon)
 			_G["AchievementFrameStatsContainerButton"..i.."HeaderMiddle"]:SetAlpha(0)
 			_G["AchievementFrameStatsContainerButton"..i.."HeaderRight"]:SetAlpha(0)
 		end
+		
+		AchievementFrameComparisonHeader:SetPoint("BOTTOMRIGHT", AchievementFrameComparison, "TOPRIGHT", 39, 25)
+		
+		local headerbg = CreateFrame("Frame", nil, AchievementFrameComparisonHeader)
+		headerbg:SetPoint("TOPLEFT", 20, -20)
+		headerbg:SetPoint("BOTTOMRIGHT", -28, -5)
+		headerbg:SetFrameLevel(AchievementFrameComparisonHeader:GetFrameLevel()-1)
+		F.CreateBD(headerbg, .25)
+		
+		local summaries = {AchievementFrameComparisonSummaryPlayer, AchievementFrameComparisonSummaryFriend}
+		
+		for _, frame in pairs(summaries) do
+			frame:SetBackdrop(nil)
+			local bg = CreateFrame("Frame", nil, frame)
+			bg:SetPoint("TOPLEFT", 2, -2)
+			bg:SetPoint("BOTTOMRIGHT", -2, 0)
+			bg:SetFrameLevel(frame:GetFrameLevel()-1)
+			F.CreateBD(bg, .25)
+		end
+
+		local bars = {AchievementFrameComparisonSummaryPlayerStatusBar, AchievementFrameComparisonSummaryFriendStatusBar}
+		
+		for _, bar in pairs(bars) do
+			local name = bar:GetName()
+			bar:SetStatusBarTexture(C.media.backdrop)
+			bar:GetStatusBarTexture():SetGradient("VERTICAL", 0, .4, 0, 0, .6, 0)
+			_G[name.."Left"]:Hide()
+			_G[name.."Middle"]:Hide()
+			_G[name.."Right"]:Hide()
+			_G[name.."FillBar"]:Hide()
+			_G[name.."Title"]:SetTextColor(1, 1, 1)
+			_G[name.."Title"]:SetPoint("LEFT", bar, "LEFT", 6, 0)
+			_G[name.."Text"]:SetPoint("RIGHT", bar, "RIGHT", -5, 0)
+			
+			local bg = CreateFrame("Frame", nil, bar)
+			bg:SetPoint("TOPLEFT", -1, 1)
+			bg:SetPoint("BOTTOMRIGHT", 1, -1)
+			bg:SetFrameLevel(bar:GetFrameLevel()-1)
+			F.CreateBD(bg, .25)
+		end
+		
+		for i = 1, 9 do
+			local buttons = {_G["AchievementFrameComparisonContainerButton"..i.."Player"], _G["AchievementFrameComparisonContainerButton"..i.."Friend"]}
+			
+			for _, button in pairs(buttons) do
+				button:DisableDrawLayer("BORDER")
+				local bg = CreateFrame("Frame", nil, button)
+				bg:SetPoint("TOPLEFT", 2, -3)
+				bg:SetPoint("BOTTOMRIGHT", -2, 2)
+				F.CreateBD(bg, 0)
+			end
+
+			local bd = _G["AchievementFrameComparisonContainerButton"..i.."PlayerBackground"]
+			bd:SetTexture(C.media.backdrop)
+			bd:SetVertexColor(0, 0, 0, .25)
+			
+			local bd = _G["AchievementFrameComparisonContainerButton"..i.."FriendBackground"]
+			bd:SetTexture(C.media.backdrop)
+			bd:SetVertexColor(0, 0, 0, .25)
+
+			local text = _G["AchievementFrameComparisonContainerButton"..i.."PlayerDescription"]
+			text:SetTextColor(.9, .9, .9)
+			text.SetTextColor = F.dummy
+			text:SetShadowOffset(1, -1)
+			text.SetShadowOffset = F.dummy
+
+			_G["AchievementFrameComparisonContainerButton"..i.."PlayerTitleBackground"]:Hide()
+			_G["AchievementFrameComparisonContainerButton"..i.."PlayerGlow"]:Hide()
+			_G["AchievementFrameComparisonContainerButton"..i.."PlayerIconOverlay"]:Hide()
+			_G["AchievementFrameComparisonContainerButton"..i.."FriendTitleBackground"]:Hide()
+			_G["AchievementFrameComparisonContainerButton"..i.."FriendGlow"]:Hide()
+			_G["AchievementFrameComparisonContainerButton"..i.."FriendIconOverlay"]:Hide()
+			
+			local ic = _G["AchievementFrameComparisonContainerButton"..i.."PlayerIconTexture"]
+			ic:SetTexCoord(.08, .92, .08, .92)
+			F.CreateBG(ic)
+			
+			local ic = _G["AchievementFrameComparisonContainerButton"..i.."FriendIconTexture"]
+			ic:SetTexCoord(.08, .92, .08, .92)
+			F.CreateBG(ic)
+		end
 
 		F.ReskinClose(AchievementFrameCloseButton)
 		F.ReskinScroll(AchievementFrameAchievementsContainerScrollBar)
 		F.ReskinScroll(AchievementFrameStatsContainerScrollBar)
 		F.ReskinScroll(AchievementFrameCategoriesContainerScrollBar)
+		F.ReskinScroll(AchievementFrameComparisonContainerScrollBar)
 		F.ReskinDropDown(AchievementFrameFilterDropDown)
 	elseif addon == "Blizzard_BarbershopUI" then
 		F.SetBD(BarberShopFrame, 44, -75, -40, 44)
