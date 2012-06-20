d1531dc09898f42ebdee9bad20dd07e3f767e399
Haleth <haleth@live.com>
2012-06-20 15:05:42 +0200

    Fix for addons which inherit GameTooltip's look.

--- aurora.lua
+++ aurora.lua
@@ -5331,6 +5331,25 @@ Delay:SetScript("OnEvent", function()
 			"VoiceMacroMenu",
 		}
 
+		local backdrop = {
+			bgFile = C.media.backdrop,
+			edgeFile = C.media.backdrop,
+			edgeSize = 1,
+		}
+
+		-- so other stuff which tries to look like GameTooltip doesn't mess up
+		local getBackdrop = function()
+			return backdrop
+		end
+
+		local getBackdropColor = function()
+			return 0, 0, 0, .6
+		end
+
+		local getBackdropBorderColor = function()
+			return 0, 0, 0
+		end
+
 		for i = 1, #tooltips do
 			local t = _G[tooltips[i]]
 			t:SetBackdrop(nil)
@@ -5338,7 +5357,13 @@ Delay:SetScript("OnEvent", function()
 			bg:SetPoint("TOPLEFT")
 			bg:SetPoint("BOTTOMRIGHT")
 			bg:SetFrameLevel(t:GetFrameLevel()-1)
-			F.CreateBD(bg, .6)
+			bg:SetBackdrop(backdrop)
+			bg:SetBackdropColor(0, 0, 0, .6)
+			bg:SetBackdropBorderColor(0, 0, 0)
+			
+			t.GetBackdrop = getBackdrop
+			t.GetBackdropColor = getBackdropColor
+			t.GetBackdropBorderColor = getBackdropBorderColor
 		end
 
 		local sb = _G["GameTooltipStatusBar"]
