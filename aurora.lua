f77fd1b1bc7326d18e0fe61d490b82678183e9ad
Haleth <haleth@live.com>
2012-06-23 19:24:24 +0200

    Fix a tradeskill frame texture.

--- aurora.lua
+++ aurora.lua
@@ -5238,6 +5238,11 @@ Skin:SetScript("OnEvent", function(self, event, addon)
 		F.CreateBD(bg, .25)
 
 		ClassTrainerFrameSkillStepButton:SetHighlightTexture(nil)
+		
+		local dis = select(6, ClassTrainerFrameSkillStepButton:GetRegions())
+		dis:Hide()
+		dis.Show = F.dummy
+		
 		select(7, ClassTrainerFrameSkillStepButton:GetRegions()):SetAlpha(0)
 
 		local check = select(4, ClassTrainerFrameSkillStepButton:GetRegions())
