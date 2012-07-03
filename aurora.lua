e181cae4957517019fc1d68b355e29857f9a22cc
Haleth <haleth@live.com>
2012-07-03 13:14:30 +0200

    Reskin mount frame.

--- aurora.lua
+++ aurora.lua
@@ -4884,6 +4884,79 @@ Skin:SetScript("OnEvent", function(self, event, addon)
 		F.ReskinScroll(MacroButtonScrollFrameScrollBar)
 		F.ReskinScroll(MacroFrameScrollFrameScrollBar)
 		F.ReskinScroll(MacroPopupScrollFrameScrollBar)
+	elseif addon == "Blizzard_PetJournal" then
+		for i = 1, 14 do
+			if i ~= 8 then
+				select(i, PetJournalParent:GetRegions()):Hide()
+			end
+		end
+		for i = 1, 9 do
+			select(i, MountJournal.MountCount:GetRegions()):Hide()
+		end
+		
+		MountJournalMountButton_RightSeparator:Hide()
+		MountJournal.LeftInset:Hide()
+		MountJournal.RightInset:Hide()
+		MountJournal.MountDisplay:GetRegions():Hide()
+		MountJournal.MountDisplay.ShadowOverlay:Hide()
+		
+		local buttons = MountJournal.ListScrollFrame.buttons
+		for i = 1, #buttons do
+			local bu = buttons[i]
+			
+			bu:GetRegions():Hide()
+			bu.selectedTexture:SetAlpha(0)
+			bu:SetHighlightTexture("")
+			
+			local bg = CreateFrame("Frame", nil, bu)
+			bg:SetPoint("TOPLEFT", 0, -1)
+			bg:SetPoint("BOTTOMRIGHT", 0, 1)
+			bg:SetFrameLevel(bu:GetFrameLevel()-1)
+			F.CreateBD(bg, .25)
+			bu.bg = bg
+			
+			bu.icon:SetTexCoord(.08, .92, .08, .92)
+			bu.icon:SetDrawLayer("OVERLAY")
+			F.CreateBG(bu.icon)
+			
+			bu.name:SetParent(bg)
+			
+			bu.DragButton:GetRegions():SetTexture(C.media.checked)
+		end
+		
+		local function updateScroll()
+			local buttons = MountJournal.ListScrollFrame.buttons
+			for i = 1, #buttons do
+				local bu = buttons[i]
+				if bu.selectedTexture:IsShown() then
+					bu.bg:SetBackdropColor(r, g, b, .25)
+				else
+					bu.bg:SetBackdropColor(0, 0, 0, .25)
+				end
+				if i == 2 then
+					bu:SetPoint("TOPLEFT", buttons[i-1], "BOTTOMLEFT", 0, -1)
+				elseif i > 2 then
+					bu:SetPoint("TOPLEFT", buttons[i-1], "BOTTOMLEFT", 0, 0)
+				end
+			end
+		end
+		
+		hooksecurefunc("MountJournal_UpdateMountList", updateScroll)
+		MountJournalListScrollFrame:HookScript("OnVerticalScroll", updateScroll)
+		MountJournalListScrollFrame:HookScript("OnMouseWheel", updateScroll)
+		
+		F.CreateBD(PetJournalParent)
+		F.CreateSD(PetJournalParent)
+		F.CreateBD(MountJournal.MountCount, .25)
+		F.CreateBD(MountJournal.MountDisplay.ModelFrame, .25)
+		
+		F.Reskin(MountJournalMountButton)
+		F.CreateTab(PetJournalParentTab1)
+		F.CreateTab(PetJournalParentTab2)
+		F.ReskinClose(PetJournalParentCloseButton)
+		F.ReskinScroll(MountJournalListScrollFrameScrollBar)
+		F.ReskinArrow(MountJournal.MountDisplay.ModelFrame.RotateLeftButton, "left")
+		F.ReskinArrow(MountJournal.MountDisplay.ModelFrame.RotateRightButton, "right")
 	elseif addon == "Blizzard_RaidUI" then
 		F.Reskin(RaidFrameReadyCheckButton)
 		F.ReskinCheck(RaidFrameAllAssistCheckButton)
