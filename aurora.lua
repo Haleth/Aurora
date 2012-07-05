1e4aeebd0f2b2f0e2af695ee6b7259cbc328f0ae
Haleth <haleth@live.com>
2012-07-05 15:32:47 +0200

    Reskin bnet conversation window.

--- aurora.lua
+++ aurora.lua
@@ -2256,6 +2256,20 @@ Skin:SetScript("OnEvent", function(self, event, addon)
 		TutorialFrameOkayButton:SetBackdropColor(0, 0, 0, .25)
 		TutorialFramePrevButton:SetBackdropColor(0, 0, 0, .25)
 		TutorialFrameNextButton:SetBackdropColor(0, 0, 0, .25)
+		
+		-- BN conversation
+		
+		BNConversationInviteDialogHeader:SetTexture("")
+		
+		F.CreateBD(BNConversationInviteDialog)
+		F.CreateBD(BNConversationInviteDialogList, .25)
+		
+		F.Reskin(BNConversationInviteDialogInviteButton)
+		F.Reskin(BNConversationInviteDialogCancelButton)
+		F.ReskinScroll(BNConversationInviteDialogListScrollFrameScrollBar)
+		for i = 1, BN_CONVERSATION_INVITE_NUM_DISPLAYED do
+			F.ReskinCheck(_G["BNConversationInviteDialogListFriend"..i].checkButton)
+		end
 
 		-- [[ Hide regions ]]
 
