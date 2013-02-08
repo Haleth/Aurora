beac43640700b0cc4848a34772365b39f5a3b88f
Haleth <haleth@live.com>
2013-02-08 14:38:57 +0100

    Fix a typo.

--- aurora.lua
+++ aurora.lua
@@ -3601,7 +3601,7 @@ Skin:SetScript("OnEvent", function(self, event, addon)
 			end
 		end)
 
-		-- Pet battle queue popoup
+		-- Pet battle queue popup
 
 		F.CreateBD(PetBattleQueueReadyFrame)
 		F.CreateSD(PetBattleQueueReadyFrame)
