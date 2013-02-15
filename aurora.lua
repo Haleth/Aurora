4464267d3b3964d68b252b8f1226723eaa6718a3
Haleth <haleth@live.com>
2013-02-15 19:09:20 +0100

    Fix static popup item pushed texture.

--- aurora.lua
+++ aurora.lua
@@ -2448,6 +2448,7 @@ Skin:SetScript("OnEvent", function(self, event, addon)
 			_G["StaticPopup"..i.."ItemFrameIconTexture"]:SetTexCoord(.08, .92, .08, .92)
 
 			bu:SetNormalTexture("")
+			bu:SetPushedTexture("")
 			F.CreateBG(bu)
 		end
 
