9a9e315c623155f0569c96136a3a0de530c89c6c
Haleth <haleth@live.com>
2013-03-30 18:06:21 +0100

    Fix first achievement button background.

--- AddOns/Blizzard_AchievementUI.lua
+++ AddOns/Blizzard_AchievementUI.lua
@@ -150,6 +150,8 @@ C.modules["Blizzard_AchievementUI"] = function()
 		bottom:SetPoint("BOTTOMRIGHT", tex, 1, -1)
 	end
 
+	AchievementFrameAchievementsContainerButton1.background:SetPoint("TOPLEFT", AchievementFrameAchievementsContainerButton1, "TOPLEFT", 2, -3)
+
 	hooksecurefunc("AchievementButton_DisplayAchievement", function(button, category, achievement)
 		local _, _, _, completed = GetAchievementInfo(category, achievement)
 		if completed then
