338976ce7abf70e4fea4fcd900b863445695bbd4
Haleth <haleth@live.com>
2013-05-03 18:24:59 +0200

    Fix talent frame taint.

--- AddOns/Blizzard_TalentUI.lua
+++ AddOns/Blizzard_TalentUI.lua
@@ -101,6 +101,34 @@ C.modules["Blizzard_TalentUI"] = function()
 		PlayerTalentFrameSpecialization["specButton"..i].specIcon:SetTexture(icon)
 	end
 
+	-- Annoying animation stuff
+
+	local function fixGradient(self)
+		select(self.numRegion, self:GetParent():GetRegions()):SetGradientAlpha("VERTICAL", 0, 0, 0, .3, .35, .35, .35, .35)
+	end
+
+	local function onPlay(self)
+		self:Stop()
+		self.numRegions = self:GetParent():GetNumRegions()
+		self:SetScript("OnUpdate", fixGradient)
+	end
+
+	local function onFinished(self)
+		self:SetScript("OnUpdate", nil)
+		fixGradient(self)
+	end
+
+	PlayerTalentFrameSpecializationLearnButton.FlashAnim.numRegion = 18
+	PlayerTalentFrameSpecializationLearnButton.FlashAnim:SetScript("OnPlay", onPlay)
+	PlayerTalentFrameSpecializationLearnButton.FlashAnim:SetScript("OnFinished", onFinished)
+	PlayerTalentFrameTalentsLearnButton.FlashAnim.numRegion = 18
+	PlayerTalentFrameTalentsLearnButton.FlashAnim:SetScript("OnPlay", onPlay)
+	PlayerTalentFrameTalentsLearnButton.FlashAnim:SetScript("OnFinished", onFinished)
+	PlayerTalentFrameSpecializationLearnButton.FlashAnim:SetLooping("NONE")
+	PlayerTalentFrameSpecializationLearnButton.Flash:SetTexture("")
+	PlayerTalentFrameTalentsLearnButton.FlashAnim:SetLooping("NONE")
+	PlayerTalentFrameTalentsLearnButton.Flash:SetTexture("")
+
 	local buttons = {"PlayerTalentFrameSpecializationSpecButton", "PlayerTalentFramePetSpecializationSpecButton"}
 
 	for _, name in pairs(buttons) do
@@ -111,9 +139,10 @@ C.modules["Blizzard_TalentUI"] = function()
 			bu.ring:Hide()
 			bu.learnedTex:SetPoint("TOPLEFT", 1, -1)
 			bu.learnedTex:SetPoint("BOTTOMRIGHT", -1, 1)
-			_G["PlayerTalentFrameSpecializationSpecButton"..i.."Glow"]:Hide()
-			_G["PlayerTalentFrameSpecializationSpecButton"..i.."Glow"].Show = F.dummy
-			bu.animLearn.Play = F.dummy
+			_G[name..i.."Glow"]:SetTexture("")
+			bu.animLearn.numRegion = 20
+			bu.animLearn:SetScript("OnPlay", onPlay)
+			bu.animLearn:SetScript("OnFinished", onFinished)
 
 			F.Reskin(bu, true)
 
@@ -217,11 +246,6 @@ C.modules["Blizzard_TalentUI"] = function()
 	PlayerTalentFrameTalentsTutorialButton:SetPoint("TOPLEFT", PlayerTalentFrame, "TOPLEFT", -12, 12)
 	PlayerTalentFrameSpecializationTutorialButton.Ring:Hide()
 	PlayerTalentFrameSpecializationTutorialButton:SetPoint("TOPLEFT", PlayerTalentFrame, "TOPLEFT", -12, 12)
-
-	PlayerTalentFrameSpecializationLearnButton.FlashAnim.Play = F.dummy
-	PlayerTalentFrameSpecializationLearnButton.Flash:SetTexture("")
-	PlayerTalentFrameTalentsLearnButton.FlashAnim.Play = F.dummy
-	PlayerTalentFrameTalentsLearnButton.Flash:SetTexture("")
 
 	F.ReskinPortraitFrame(PlayerTalentFrame, true)
 	F.Reskin(PlayerTalentFrameSpecializationLearnButton)
