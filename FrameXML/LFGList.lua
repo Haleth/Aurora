65f217f57c931c5bd5d782b419495a2d96f8f20c
Haleth <haleth@telenet.be>
2014-11-07 16:20:18 +0100

    Remove LFG list "Nothing available" borders.

--- FrameXML/LFGList.lua
+++ FrameXML/LFGList.lua
@@ -35,6 +35,12 @@ tinsert(C.themes["Aurora"], function()
 		end
 	end)
 
+	-- [[ Nothing available ]]
+
+	local NothingAvailable = LFGListFrame.NothingAvailable
+
+	NothingAvailable.Inset:DisableDrawLayer("BORDER")
+
 	-- [[ Search panel ]]
 
 	local SearchPanel = LFGListFrame.SearchPanel
