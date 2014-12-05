b1ed30c1aed85e779c0f9e5d57541fea7643a736
Haleth <haleth@telenet.be>
2014-12-05 22:04:14 +0100

    Move out garrison portrait API.

--- aurora.lua
+++ aurora.lua
@@ -643,6 +643,34 @@ F.ReskinNavBar = function(f)
 	overflowButton:HookScript("OnLeave", clearArrow)
 end
 
+F.ReskinGarrisonPortrait = function(portrait)
+	local level = portrait.Level
+	local cover = portrait.PortraitRingCover
+
+	portrait.PortraitRing:Hide()
+	portrait.PortraitRingQuality:SetTexture("")
+
+	portrait.LevelBorder:SetTexture(0, 0, 0, .5)
+	portrait.LevelBorder:SetSize(44, 11)
+	portrait.LevelBorder:ClearAllPoints()
+	portrait.LevelBorder:SetPoint("BOTTOM", 0, 12)
+
+	level:ClearAllPoints()
+	level:SetPoint("BOTTOM", portrait, 0, 12)
+
+	local squareBG = CreateFrame("Frame", nil, portrait)
+	squareBG:SetFrameLevel(portrait:GetFrameLevel()-1)
+	squareBG:SetPoint("TOPLEFT", 3, -3)
+	squareBG:SetPoint("BOTTOMRIGHT", -3, 11)
+	F.CreateBD(squareBG, 1)
+	portrait.squareBG = squareBG
+
+	if cover then
+		cover:SetTexture(0, 0, 0)
+		cover:SetAllPoints(squareBG)
+	end
+end
+
 -- [[ Variable and module handling ]]
 
 C.themes = {}
