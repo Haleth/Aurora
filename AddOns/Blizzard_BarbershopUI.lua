5addb67308c31b7556318e19aaaed74fa7d1d8d5
Gethe <gethe88@gmail.com>
2016-08-12 22:51:38 -0500

    Update BarbershopUI

--- AddOns/Blizzard_BarbershopUI.lua
+++ AddOns/Blizzard_BarbershopUI.lua
@@ -6,26 +6,28 @@ local F, C = _G.unpack(_G.select(2, ...))
 
 C.themes["Blizzard_BarbershopUI"] = function()
 	local BarberShopFrame = _G.BarberShopFrame
-	BarberShopFrame:GetRegions():Hide()
-	_G.BarberShopFrameMoneyFrame:GetRegions():Hide()
-	_G.BarberShopAltFormFrameBackground:Hide()
-	_G.BarberShopAltFormFrameBorder:Hide()
-
-	_G.BarberShopAltFormFrame:ClearAllPoints()
-	_G.BarberShopAltFormFrame:SetPoint("BOTTOM", BarberShopFrame, "TOP", 0, -74)
-
+	for i = 1, BarberShopFrame:GetNumRegions() do
+		local region = _G.select(i, BarberShopFrame:GetRegions())
+		region:Hide()
+	end
 	F.SetBD(BarberShopFrame, 44, -75, -40, 44)
-	F.SetBD(_G.BarberShopAltFormFrame, 0, 0, 2, -2)
-
-	F.Reskin(_G.BarberShopFrameOkayButton)
-	F.Reskin(_G.BarberShopFrameCancelButton)
-	F.Reskin(_G.BarberShopFrameResetButton)
 
 	for i = 1, #BarberShopFrame.Selector do
 		local prevBtn, nextBtn = BarberShopFrame.Selector[i]:GetChildren()
 		F.ReskinArrow(prevBtn, "left")
 		F.ReskinArrow(nextBtn, "right")
 	end
+
+	_G.BarberShopFrameMoneyFrame:GetRegions():Hide()
+	F.Reskin(_G.BarberShopFrameOkayButton)
+	F.Reskin(_G.BarberShopFrameCancelButton)
+	F.Reskin(_G.BarberShopFrameResetButton)
+
+	F.SetBD(_G.BarberShopAltFormFrame, 0, 0, 2, -2)
+	_G.BarberShopAltFormFrame:ClearAllPoints()
+	_G.BarberShopAltFormFrame:SetPoint("BOTTOM", BarberShopFrame, "TOP", 0, -74)
+	_G.BarberShopAltFormFrameBackground:Hide()
+	_G.BarberShopAltFormFrameBorder:Hide()
 
 	-- [[ Banner frame ]]
 
