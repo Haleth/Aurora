9996b8c11b7dda50bea2b28e1129aeb1fe33e9bb
Gethe <gethe88@gmail.com>
2017-04-18 20:13:58 -0500

    Skin EventTrace

--- AddOns/Blizzard_DebugTools.lua
+++ AddOns/Blizzard_DebugTools.lua
@@ -19,12 +19,38 @@ C.themes["Blizzard_DebugTools"] = function()
 		F.ReskinClose(_G.ScriptErrorsFrameClose)
 	end
 
+    --[[ EventTrace ]]--
+    for i = 1, _G.EventTraceFrame:GetNumRegions() do
+        local region = _G.select(i, _G.EventTraceFrame:GetRegions())
+        if region:GetObjectType() == "Texture" then
+            region:SetTexture(nil)
+        end
+    end
+    _G.EventTraceFrame:SetHeight(600)
+    F.CreateBD(_G.EventTraceFrame)
+    F.ReskinClose(_G.EventTraceFrameCloseButton)
+
+    _G.EventTraceFrameTitleButton:ClearAllPoints()
+    _G.EventTraceFrameTitleButton:SetPoint("TOPLEFT")
+    _G.EventTraceFrameTitleButton:SetPoint("BOTTOMRIGHT", _G.EventTraceFrame, "TOPRIGHT", 0, -24)
+
+    _G.EventTraceFrameScrollBG:Hide()
+    local thumb = _G.EventTraceFrameScroll.thumb
+    thumb:SetAlpha(0)
+    thumb:SetWidth(17)
+    local etraceBG = _G.CreateFrame("Frame", nil, _G.EventTraceFrameScroll)
+    etraceBG:SetPoint("TOPLEFT", thumb, 0, 0)
+    etraceBG:SetPoint("BOTTOMRIGHT", thumb, 0, 0)
+    F.CreateBD(etraceBG, 0)
+    F.CreateGradient(etraceBG)
+
+    --[[ FrameStack ]]--
 	_G.FrameStackTooltip:SetScale(_G.UIParent:GetScale())
 	_G.FrameStackTooltip:SetBackdrop(nil)
 
-	local bg = _G.CreateFrame("Frame", nil, _G.FrameStackTooltip)
-	bg:SetPoint("TOPLEFT")
-	bg:SetPoint("BOTTOMRIGHT")
-	bg:SetFrameLevel(_G.FrameStackTooltip:GetFrameLevel()-1)
-	F.CreateBD(bg, .6)
+	local fstackBG = _G.CreateFrame("Frame", nil, _G.FrameStackTooltip)
+	fstackBG:SetFrameLevel(_G.FrameStackTooltip:GetFrameLevel()-1)
+	fstackBG:SetPoint("TOPLEFT")
+	fstackBG:SetPoint("BOTTOMRIGHT")
+	F.CreateBD(fstackBG, .6)
 end
