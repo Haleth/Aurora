2c0b607b42d1bf041281ef497b37854abe188733
Gethe <gethe88@gmail.com>
2017-05-16 20:25:48 -0500

    Update BattlefieldMinimap skin

--- AddOns/Blizzard_BattlefieldMinimap.lua
+++ AddOns/Blizzard_BattlefieldMinimap.lua
@@ -4,8 +4,21 @@ local _, private = ...
 local F, C = _G.unpack(private.Aurora)
 
 C.themes["Blizzard_BattlefieldMinimap"] = function()
-	F.SetBD(_G.BattlefieldMinimap, -1, 1, -5, 3)
+    _G.BattlefieldMinimapTab:DisableDrawLayer("BACKGROUND")
+
+	local bg = F.CreateBDFrame(_G.BattlefieldMinimap, 1.0 - _G.BattlefieldMinimapOptions.opacity)
+    _G.BattlefieldMinimap:SetPoint("TOPLEFT", _G.BattlefieldMinimapTab, "BOTTOMLEFT")
+    bg:SetPoint("TOPLEFT", -1, 1)
+    bg:SetPoint("BOTTOMRIGHT", -5, 3)
+    local r, g, b = bg:GetBackdropColor()
+
 	_G.BattlefieldMinimapCorner:Hide()
 	_G.BattlefieldMinimapBackground:Hide()
 	_G.BattlefieldMinimapCloseButton:Hide()
+
+    _G.hooksecurefunc("BattlefieldMinimap_UpdateOpacity", function()
+        local alpha = 1.0 - _G.BattlefieldMinimapOptions.opacity
+        bg:SetBackdropColor(r, g, b, alpha)
+        bg:SetBackdropBorderColor(r, g, b, alpha)
+    end)
 end
