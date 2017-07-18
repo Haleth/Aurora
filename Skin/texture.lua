a1a6107f551ddc2fd64391981d22879c69e6e07a
Gethe <gethe88@gmail.com>
2017-07-18 12:31:14 -0500

    Add an assert

--- Skin/texture.lua
+++ Skin/texture.lua
@@ -84,6 +84,7 @@ local snapshots = {}
 
 function Base.SetTexture(texture, textureName, useTextureSize)
     local snapshot = snapshots[textureName]
+    _G.assert(snapshot, textureName .. " is not a registered texture.")
     if not snapshot.id or not SnapshotFrame:IsSnapshotValid(snapshot.id) then
         snapshot.create(textureFrame)
         local width, height = textureFrame:GetSize()
