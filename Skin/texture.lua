f20184649dd32008de30c8ad1054051f80c2cd86
Gethe <gethe88@gmail.com>
2017-08-03 13:45:09 -0500

    Split up lfgIcons into separate icons
    Snapshot textures don't respond to SetTexCoord properly

--- Skin/texture.lua
+++ Skin/texture.lua
@@ -79,54 +79,41 @@ end
 
 do -- LFG Icons
     local map = {
-        {1, 1},
-        {2, 1},
-        {3, 1},
-        {1, 2},
-        {2, 2},
-        {3, 2},
-        {1, 3},
-        {2, 3},
+        {name = "GUIDE", 1, 1},
+        {name = "HEALER", 2, 1},
+        {name = "CHECK", 3, 1},
+        {name = "TANK", 1, 2},
+        {name = "DAMAGER", 2, 2},
+        {name = "PROMPT", 3, 2},
+        {name = "COVER", 1, 3},
+        {name = "CROSS", 2, 3},
     }
-    local function CreateIcon(frame, i)
+    local size = 76
+    for i = 1, #map do
         local info = map[i]
-        local icon = frame:CreateTexture(nil, "BORDER")
-        icon:SetSize(67, 67)
-        icon:SetTexture([[Interface\LFGFrame\UI-LFG-ICON-ROLES]])
-        icon:SetTexCoord(_G.GetTexCoordsByGrid(info[1], info[2], 256, 256, 67, 67))
-        icon:Show()
+        Base.RegisterTexture("role"..info.name, function(frame)
+            frame:SetSize(64, 64)
 
-        local mask = frame:CreateMaskTexture(nil, "BORDER")
-        mask:SetTexture([[Interface\CharacterFrame\TempPortraitAlphaMask]], "CLAMPTOBLACKADDITIVE", "CLAMPTOBLACKADDITIVE")
-        mask:SetSize(47, 47)
-        mask:SetPoint("TOPLEFT", icon, 10, -8)
-        mask:Show()
-        icon:AddMaskTexture(mask)
-        return icon
+            local bg = frame:CreateTexture(nil, "BACKGROUND")
+            bg:SetAllPoints()
+            bg:SetColorTexture(0, 0, 0)
+            bg:Show()
+
+            local icon = frame:CreateTexture(nil, "BORDER")
+            icon:SetSize(size, size)
+            icon:SetPoint("CENTER", 2, -1)
+            icon:SetTexture([[Interface\LFGFrame\UI-LFG-ICON-ROLES]])
+            icon:SetTexCoord(_G.GetTexCoordsByGrid(info[1], info[2], 256, 256, 67, 67))
+            icon:Show()
+
+            local mask = frame:CreateMaskTexture(nil, "BORDER")
+            mask:SetTexture([[Interface\CharacterFrame\TempPortraitAlphaMask]], "CLAMPTOBLACKADDITIVE", "CLAMPTOBLACKADDITIVE")
+            mask:SetSize(size * 0.7, size * 0.7)
+            mask:SetPoint("CENTER", icon, 0, 2)
+            mask:Show()
+            icon:AddMaskTexture(mask)
+        end)
     end
-    Base.RegisterTexture("lfgIcons", function(frame)
-        frame:SetSize(256, 256)
-
-        local bg = frame:CreateTexture(nil, "BACKGROUND")
-        bg:SetAllPoints()
-        bg:SetColorTexture(0, 0, 0)
-        bg:Show()
-
-        local prevIcon = CreateIcon(frame, 1)
-        local cornerIcon = prevIcon
-        prevIcon:SetPoint("TOPLEFT", frame)
-
-        for i = 2, #map do
-            local icon = CreateIcon(frame, i)
-            if i % 3 == 1 then
-                icon:SetPoint("TOPLEFT", cornerIcon, "BOTTOMLEFT")
-                cornerIcon = icon
-            else
-                icon:SetPoint("TOPLEFT", prevIcon, "TOPRIGHT")
-            end
-            prevIcon = icon
-        end
-    end)
 end
 
 
@@ -146,7 +133,7 @@ local snapshot = _G.UIParent:CreateTexture("$parentSnapshotTest", "BACKGROUND")
 snapshot:SetPoint("CENTER")
 snapshot:SetSize(16, 16)
 Base.SetTexture(snapshot, "gradientLeft", true)
-Base.SetTexture(snapshot, "lfgIcons", true)
+Base.SetTexture(snapshot, "roleDAMAGER", true)
 Base.SetTexture(snapshot, "gradientUp", true)
 Base.SetTexture(snapshot, "arrowLeft", true)
 Base.SetTexture(snapshot, "gradientLeft", true)
