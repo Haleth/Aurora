64bf58ada43a25b5781628c0477a5c79920d3cb6
Tim West <gethe88@gmail.com>
2018-05-29 12:53:59 -0500

    Make frame titles larger

--- Skin/FrameXML/Constants.lua
+++ Skin/FrameXML/Constants.lua
@@ -4,7 +4,7 @@ local Color = private.Aurora.Color
 private.NORMAL_QUEST_DISPLAY = _G.NORMAL_QUEST_DISPLAY:gsub("ff000000", Color.white.colorStr)
 private.TRIVIAL_QUEST_DISPLAY = _G.TRIVIAL_QUEST_DISPLAY:gsub("ff000000", Color.grayLight.colorStr)
 
-private.FRAME_TITLE_HEIGHT = 23
+private.FRAME_TITLE_HEIGHT = 27
 
 function private.FrameXML.Constants()
 end
