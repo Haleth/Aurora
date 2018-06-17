7a3fbb37a12a76975896a19b877aebd5d52f53f7
Tim West <gethe88@gmail.com>
2018-06-16 22:49:35 -0500

    Fix error on Live

--- Skin/FrameXML/StaticPopup.lua
+++ Skin/FrameXML/StaticPopup.lua
@@ -131,7 +131,9 @@ do --[[ FrameXML\StaticPopup.xml ]]
         Skin.StaticPopupButtonTemplate(Frame.button2)
         Skin.StaticPopupButtonTemplate(Frame.button3)
         Skin.StaticPopupButtonTemplate(Frame.button4)
-        Skin.StaticPopupButtonTemplate(Frame.extraButton)
+        if private.isPatch then
+            Skin.StaticPopupButtonTemplate(Frame.extraButton)
+        end
 
         local EditBox = _G[name .. "EditBox"]
         EditBox.Left = _G[name .. "EditBoxLeft"]
