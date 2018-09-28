b1c26396b3081eb3280c299d9a39d283cbb6cc21
Tim West <gethe88@gmail.com>
2018-09-27 23:00:54 -0500

    Update AddonList

--- Skin/SharedXML/AddonList.lua
+++ Skin/SharedXML/AddonList.lua
@@ -1,29 +1,62 @@
 local _, private = ...
 
--- [[ Core ]]
-local F = _G.unpack(private.Aurora)
+--[[ Lua Globals ]]
+-- luacheck: globals
+
+--[[ Core ]]
+local Aurora = private.Aurora
+local Hook, Skin = Aurora.Hook, Aurora.Skin
+
+do --[[ SharedXML\AddonList.lua ]]
+    function Hook.AddonListCharacterDropDownButton_OnClick(self)
+        for i = 1, 2 do
+            local buttonName = "DropDownList1Button"..i
+            local button = _G[buttonName]
+            local checkBox = button._auroraCheckBox
+            local check = checkBox.check
+
+            checkBox:SetSize(8, 8)
+            checkBox:SetPoint("LEFT", 4, 0)
+            check:SetTexture([[Interface\Buttons\WHITE8x8]])
+            check:SetSize(6, 6)
+            check:SetAlpha(0.6)
+
+            local checked = _G["DropDownList1Button"..i].checked
+            if checked then
+                check:Show()
+            else
+                check:Hide()
+            end
+
+            _G[buttonName.."UnCheck"]:Hide()
+        end
+    end
+end
+
+do --[[ SharedXML\AddonList.xml ]]
+    function Skin.AddonListEntryTemplate(Button)
+        local name = Button:GetName()
+
+        Skin.UICheckButtonTemplate(_G[name.."Enabled"]) -- BlizzWTF: Doesn't use a template, but it should
+        Skin.UIPanelButtonTemplate(Button.LoadAddonButton)
+    end
+end
 
 function private.SharedXML.AddonList()
-    F.ReskinPortraitFrame(_G.AddonList, true)
-    F.Reskin(_G.AddonListEnableAllButton)
-    F.Reskin(_G.AddonListDisableAllButton)
-    F.Reskin(_G.AddonListCancelButton)
-    F.Reskin(_G.AddonListOkayButton)
-    F.ReskinCheck(_G.AddonListForceLoad)
-    F.ReskinDropDown(_G.AddonCharacterDropDown)
-    F.ReskinScroll(_G.AddonListScrollFrameScrollBar)
+    local AddonList = _G.AddonList
+    Skin.ButtonFrameTemplate(AddonList)
+    Skin.UICheckButtonTemplate(_G.AddonListForceLoad) -- BlizzWTF: Doesn't use a template, but it should
 
-    _G.AddonCharacterDropDown:SetWidth(170)
+    Skin.MagicButtonTemplate(AddonList.CancelButton)
+    Skin.MagicButtonTemplate(AddonList.OkayButton)
+    Skin.MagicButtonTemplate(AddonList.EnableAllButton)
+    Skin.MagicButtonTemplate(AddonList.DisableAllButton)
+    Skin.MagicButtonTemplate(AddonList.CancelButton)
 
-    _G.hooksecurefunc("AddonList_Update", function()
-        for i = 1, _G.MAX_ADDONS_DISPLAYED do
-            local checkbox = _G["AddonListEntry"..i.."Enabled"]
-            if not checkbox.isSkinned then
-                F.ReskinCheck(checkbox, true)
-                F.Reskin(_G["AddonListEntry"..i.."Load"])
-                checkbox.isSkinned = true
-            end
-            checkbox:SetTriState(checkbox.state)
-        end
-    end)
+    for i = 1, _G.MAX_ADDONS_DISPLAYED do
+        Skin.AddonListEntryTemplate(_G["AddonListEntry"..i])
+    end
+
+    Skin.UIDropDownMenuTemplate(_G.AddonCharacterDropDown)
+    _G.AddonCharacterDropDown.Button:HookScript("OnClick", Hook.AddonListCharacterDropDownButton_OnClick)
 end
--- Skin/SharedXML/Util.lua
+++ Skin/SharedXML/Util.lua
@@ -1,6 +1,38 @@
 local _, private = ...
 
+--[[ Lua Globals ]]
+-- luacheck: globals
+
+--[[ Core ]]
+local Aurora = private.Aurora
+local Hook = Aurora.Hook
+local Color = Aurora.Color
+
+do --[[ SharedXML\Util.lua ]]
+    function Hook.TriStateCheckbox_SetState(checked, checkButton)
+        local checkedTexture = _G[checkButton:GetName().."CheckedTexture"]
+        if not checked or checked == 0 then
+            -- nil or 0 means not checked
+            checkButton:SetChecked(false)
+        else
+            checkedTexture:SetDesaturated(true)
+            checkButton:SetChecked(true)
+
+            local red, green, blue = Color.highlight:GetRGB()
+            if checked == 2 then
+                -- 2 is a normal check
+                checkedTexture:SetVertexColor(red, green, blue)
+            else
+                -- 1 is a dark check
+                checkedTexture:SetVertexColor(red * 0.5, green * 0.5, blue * 0.5)
+            end
+        end
+    end
+end
+
 function private.SharedXML.Util()
+    _G.hooksecurefunc("TriStateCheckbox_SetState", Hook.TriStateCheckbox_SetState)
+
     if private.classColorsInit then
         private.classColorsInit()
     end
