cc22aaf9797639d4350ccd0126ffd323fa3e6262
Tim West <gethe88@gmail.com>
2018-11-25 11:57:08 -0600

    Rename BG map

--- Skin/AddOns/Blizzard_BattlefieldMap.lua
+++ Skin/AddOns/Blizzard_BattlefieldMap.lua
@@ -53,24 +53,3 @@ function private.AddOns.Blizzard_BattlefieldMap()
 
     --[[ Scale ]]--
 end
-
-local F = _G.unpack(private.Aurora)
-function private.AddOns.Blizzard_BattlefieldMinimap()
-    _G.BattlefieldMinimapTab:DisableDrawLayer("BACKGROUND")
-
-    local bg = F.CreateBDFrame(_G.BattlefieldMinimap, 1.0 - _G.BattlefieldMinimapOptions.opacity)
-    _G.BattlefieldMinimap:SetPoint("TOPLEFT", _G.BattlefieldMinimapTab, "BOTTOMLEFT")
-    bg:SetPoint("TOPLEFT", -1, 1)
-    bg:SetPoint("BOTTOMRIGHT", -5, 3)
-    local r, g, b = bg:GetBackdropColor()
-
-    _G.BattlefieldMinimapCorner:Hide()
-    _G.BattlefieldMinimapBackground:Hide()
-    _G.BattlefieldMinimapCloseButton:Hide()
-
-    _G.hooksecurefunc("BattlefieldMinimap_UpdateOpacity", function()
-        local alpha = 1.0 - _G.BattlefieldMinimapOptions.opacity
-        bg:SetBackdropColor(r, g, b, alpha)
-        bg:SetBackdropBorderColor(r, g, b, alpha)
-    end)
-end
