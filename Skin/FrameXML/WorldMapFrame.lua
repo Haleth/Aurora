2133da0a8231f0832ea14155c8606e189900fac9
Gethe <gethe88@gmail.com>
2019-01-14 01:34:26 -0600

    Skin bounty board help plate

--- Skin/FrameXML/WorldMapFrame.lua
+++ Skin/FrameXML/WorldMapFrame.lua
@@ -4,14 +4,35 @@ local _, private = ...
 -- luacheck: globals select
 
 -- [[ Core ]]
-local Skin = private.Aurora.Skin
+local Aurora = private.Aurora
+local Hook, Skin = Aurora.Hook, Aurora.Skin
+local Util = Aurora.Util
+
+do --[[ FrameXML\WorldMapBountyBoard.lua ]]
+    local WorldMapBountyBoardMixin do
+        WorldMapBountyBoardMixin = {}
+        function WorldMapBountyBoardMixin:TryShowingIntroTutorial()
+            if self:GetDisplayLocation() == _G.LE_MAP_OVERLAY_DISPLAY_LOCATION_TOP_RIGHT or self:GetDisplayLocation() == _G.LE_MAP_OVERLAY_DISPLAY_LOCATION_BOTTOM_RIGHT then
+                Skin.GlowBoxArrowTemplate(self.TutorialBox, "Right")
+            else
+                Skin.GlowBoxArrowTemplate(self.TutorialBox, "Left")
+            end
+        end
+        function WorldMapBountyBoardMixin:TryShowingCompletionTutorial()
+            Skin.GlowBoxArrowTemplate(self.TutorialBox, "Down")
+        end
+    end
+    Hook.WorldMapBountyBoardMixin = WorldMapBountyBoardMixin
+end
 
 do --[[ FrameXML\WorldMapFrameTemplates.xml ]]
     function Skin.WorldMapBountyBoardTemplate(Frame)
+        Skin.GlowBoxFrame(Frame.TutorialBox, "Right")
     end
     function Skin.WorldMapActionButtonTemplate(Frame)
     end
 end
 
 function private.FrameXML.WorldMapFrame()
+    Util.Mixin(_G.WorldMapBountyBoardMixin, Hook.WorldMapBountyBoardMixin)
 end
