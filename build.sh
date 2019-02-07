4d606396d3ecaa86a740b8e4e838e216e8f5f986
Gethe <gethe88@gmail.com>
2019-02-07 00:44:25 -0600

    Add custom build script

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# set git identity
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+
+# clone the wiki repo
+git clone "https://github.com/Haleth/Aurora.git.wiki" .wiki
+
+# run the python script
+python parse.py -o ".wiki"
+
+# navigate to the output directory
+cd .wiki
+
+# stage changes
+git add .
+
+# commit using the last commit SHA as the message
+git commit -m "$TRAVIS_BUILD_ID"
+
+# push the wiki repo
+git push "https://${GITHUB_OAUTH}@github.com/Haleth/Aurora.git" master > /dev/null 2>&1
