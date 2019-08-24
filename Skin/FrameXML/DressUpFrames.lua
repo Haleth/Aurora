403273c94916655b8b41b80765f13372942b60d4
Gethe <gethe88@gmail.com>
2019-08-23 20:46:06 -0500

    Update dress up frame

--- Skin/FrameXML/DressUpFrames.lua
+++ Skin/FrameXML/DressUpFrames.lua
@@ -21,17 +21,22 @@ function private.FrameXML.DressUpFrames()
     local SideDressUpFrame = _G.SideDressUpFrame
     Base.SetBackdrop(SideDressUpFrame)
 
-    local top, bottom, left, right = _G.SideDressUpFrame:GetRegions()
+    local top, bottom, left, right = SideDressUpFrame:GetRegions()
     top:Hide()
     bottom:Hide()
     left:Hide()
     right:Hide()
 
     if private.isPatch then
+        SideDressUpFrame.ModelScene:SetPoint("TOPLEFT")
+        SideDressUpFrame.ModelScene:SetPoint("BOTTOMRIGHT")
         Skin.UIPanelButtonTemplate(SideDressUpFrame.ResetButton)
         Skin.UIPanelCloseButton(_G.SideDressUpFrameCloseButton)
         select(5, _G.SideDressUpFrameCloseButton:GetRegions()):Hide()
     else
+        _G.SideDressUpModel:SetPoint("TOPLEFT")
+        _G.SideDressUpModel:SetPoint("BOTTOMRIGHT")
+        _G.SideDressUpModel.controlFrame:SetPoint("TOP", 0, -5)
         Skin.UIPanelButtonTemplate(_G.SideDressUpModel.ResetButton)
         Skin.UIPanelCloseButton(_G.SideDressUpModelCloseButton)
         select(5, _G.SideDressUpModelCloseButton:GetRegions()):Hide()
@@ -45,16 +50,25 @@ function private.FrameXML.DressUpFrames()
     Skin.ButtonFrameTemplate(DressUpFrame)
     Skin.WardrobeOutfitDropDownTemplate(DressUpFrame.OutfitDropDown)
     Skin.MaximizeMinimizeButtonFrameTemplate(DressUpFrame.MaxMinButtonFrame)
+
+    if private.isPatch then
+        DressUpFrame.ModelScene:SetPoint("TOPLEFT")
+        DressUpFrame.ModelScene:SetPoint("BOTTOMRIGHT")
+    else
+        DressUpFrame.DressUpModel:SetPoint("TOPLEFT")
+        DressUpFrame.DressUpModel:SetPoint("BOTTOMRIGHT")
+        DressUpFrame.DressUpModel.controlFrame:SetPoint("TOP", 0, -65)
+    end
+
     Skin.UIPanelButtonTemplate(_G.DressUpFrameCancelButton)
+    _G.DressUpFrameCancelButton:SetFrameLevel(_G.DressUpFrameCancelButton:GetFrameLevel() + 1)
     Skin.UIPanelButtonTemplate(DressUpFrame.ResetButton)
+    DressUpFrame.ResetButton:SetFrameLevel(DressUpFrame.ResetButton:GetFrameLevel() + 1)
     Util.PositionRelative("BOTTOMRIGHT", DressUpFrame, "BOTTOMRIGHT", -15, 15, 5, "Left", {
         _G.DressUpFrameCancelButton,
         DressUpFrame.ResetButton,
     })
 
     DressUpFrame.ModelBackground:SetDrawLayer("BACKGROUND", 3)
-    DressUpFrame.ModelBackground:ClearAllPoints()
-    DressUpFrame.ModelBackground:SetPoint("TOPLEFT")
-    DressUpFrame.ModelBackground:SetPoint("BOTTOMRIGHT")
     DressUpFrame.ModelBackground:SetAlpha(0.6)
 end
