80e115c103a9410cf3accd2935076b4c37158c92
Gethe <gethe88@gmail.com>
2020-01-09 12:28:09 -0600

    Tweak QuickJoinFrame

--- Skin/FrameXML/QuickJoin.lua
+++ Skin/FrameXML/QuickJoin.lua
@@ -4,13 +4,13 @@ local _, private = ...
 -- luacheck: globals
 
 --[[ Core ]]
-local F = _G.unpack(private.Aurora)
+local Aurora = private.Aurora
+local Skin = Aurora.Skin
 
 function private.FrameXML.QuickJoin()
     local QuickJoinFrame = _G.QuickJoinFrame
-    _G.QuickJoinScrollFrameTop:Hide()
-    _G.QuickJoinScrollFrameMiddle:Hide()
-    _G.QuickJoinScrollFrameBottom:Hide()
-    F.ReskinScroll(QuickJoinFrame.ScrollFrame.scrollBar)
-    F.Reskin(QuickJoinFrame.JoinQueueButton)
+    Skin.FriendsFrameScrollFrame(QuickJoinFrame.ScrollFrame)
+    QuickJoinFrame.ScrollFrame:SetPoint("TOPLEFT", 8, -(private.FRAME_TITLE_HEIGHT + 5))
+    QuickJoinFrame.ScrollFrame:SetPoint("BOTTOMRIGHT", -28, 30)
+    Skin.MagicButtonTemplate(QuickJoinFrame.JoinQueueButton)
 end
