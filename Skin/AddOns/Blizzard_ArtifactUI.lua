ab3b73835dd1a9c98c3c445c0d5ca3bb151c6f6b
Gethe <gethe88@gmail.com>
2020-01-14 08:29:38 -0600

    Add file for artifact UI

---
+++ Skin/AddOns/Blizzard_ArtifactUI.lua
@@ -0,0 +1,39 @@
+--local _, private = ...
+
+--[[ Lua Globals ]]
+-- luacheck: globals
+
+--[[ Core ]]
+--local Aurora = private.Aurora
+--local Base = Aurora.Base
+--local Hook, Skin = Aurora.Hook, Aurora.Skin
+--local Color, Util = Aurora.Color, Aurora.Util
+
+--do --[[ AddOns\Blizzard_ArtifactUI.lua ]]
+--end
+
+--do --[[ AddOns\Blizzard_ArtifactUI.xml ]]
+--end
+
+--function private.AddOns.Blizzard_ArtifactUI()
+    ----====####$$$$%%%%$$$$####====----
+    --  Blizzard_ArtifactPowerButton  --
+    ----====####$$$$%%%%$$$$####====----
+
+
+    ----====####$$$$%%%%$$$$####====----
+    --     Blizzard_ArtifactPerks     --
+    ----====####$$$$%%%%$$$$####====----
+
+
+    ----====####$$$$%%%%$$$$####====----
+    --  Blizzard_ArtifactAppearances  --
+    ----====####$$$$%%%%$$$$####====----
+
+
+    ----====####$$$$%%%%%$$$$####====----
+    --       Blizzard_ArtifactUI       --
+    ----====####$$$$%%%%%$$$$####====----
+
+
+--end
