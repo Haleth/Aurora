f24fb9da1a029b4cb83932e4feab2b0f78396edd
Gethe <gethe88@gmail.com>
2020-03-25 22:59:07 -0500

    Hook options dropdowns

--- Skin/FrameXML/UIOptions.lua
+++ Skin/FrameXML/UIOptions.lua
@@ -9,4 +9,6 @@ local Hook = Aurora.Hook
 
 function private.FrameXML.UIOptions()
     _G.hooksecurefunc("VideoOptionsDropDownMenu_AddButton", Hook.UIDropDownMenu_AddButton)
+    _G.hooksecurefunc("VideoOptionsDropDownMenu_DisableDropDown", Hook.UIDropDownMenu_DisableDropDown)
+    _G.hooksecurefunc("VideoOptionsDropDownMenu_EnableDropDown", Hook.UIDropDownMenu_EnableDropDown)
 end
