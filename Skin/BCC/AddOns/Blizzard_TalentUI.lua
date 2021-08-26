3917d1c6823b8646a148e0aa4f34d6513f0d3bb7
JLittle <jack.little212@gmail.com>
2021-08-26 17:05:03 -0500

    Fix skinning of PlayerTalentFrame.

--- Skin/BCC/AddOns/Blizzard_TalentUI.lua
+++ Skin/BCC/AddOns/Blizzard_TalentUI.lua
@@ -30,7 +30,7 @@ do --[[ AddOns\Blizzard_TalentUI.xml ]]
 end
 
 function private.AddOns.Blizzard_TalentUI()
-    local TalentFrame = _G.TalentFrame
+    local TalentFrame = _G.PlayerTalentFrame
     Skin.FrameTypeFrame(TalentFrame)
     TalentFrame:SetBackdropOption("offsets", {
         left = 14,
@@ -67,7 +67,7 @@ function private.AddOns.Blizzard_TalentUI()
         },
     }
     for name, info in next, textures do
-        local tex = _G["TalentFrameBackground"..name]
+        local tex = _G["PlayerTalentFrameBackground"..name]
         if info.point then
             tex:SetPoint(info.point, bg)
         end
@@ -76,41 +76,41 @@ function private.AddOns.Blizzard_TalentUI()
         tex:SetAlpha(0.7)
     end
 
-    _G.TalentFrameTitleText:ClearAllPoints()
-    _G.TalentFrameTitleText:SetPoint("TOPLEFT", bg)
-    _G.TalentFrameTitleText:SetPoint("BOTTOMRIGHT", bg, "TOPRIGHT", 0, -private.FRAME_TITLE_HEIGHT)
+    _G.PlayerTalentFrameTitleText:ClearAllPoints()
+    _G.PlayerTalentFrameTitleText:SetPoint("TOPLEFT", bg)
+    _G.PlayerTalentFrameTitleText:SetPoint("BOTTOMRIGHT", bg, "TOPRIGHT", 0, -private.FRAME_TITLE_HEIGHT)
 
-    _G.TalentFramePointsLeft:Hide()
-    _G.TalentFramePointsMiddle:Hide()
-    _G.TalentFramePointsRight:Hide()
+    _G.PlayerTalentFramePointsLeft:Hide()
+    _G.PlayerTalentFramePointsMiddle:Hide()
+    _G.PlayerTalentFramePointsRight:Hide()
 
-    _G.TalentFrameSpentPoints:SetPoint("TOP", _G.TalentFrameTitleText, "BOTTOM", 0, 5)
+    _G.PlayerTalentFrameSpentPoints:SetPoint("TOP", _G.PlayerTalentFrameTitleText, "BOTTOM", 0, 5)
 
-    Skin.UIPanelCloseButton(_G.TalentFrameCloseButton)
-    Skin.UIPanelButtonTemplate(_G.TalentFrameCancelButton)
+    Skin.UIPanelCloseButton(_G.PlayerTalentFrameCloseButton)
+    Skin.UIPanelButtonTemplate(_G.PlayerTalentFrameCancelButton)
 
-    Skin.TalentTabTemplate(_G.TalentFrameTab1)
-    Skin.TalentTabTemplate(_G.TalentFrameTab2)
-    Skin.TalentTabTemplate(_G.TalentFrameTab3)
-    Skin.TalentTabTemplate(_G.TalentFrameTab4)
-    Skin.TalentTabTemplate(_G.TalentFrameTab5)
+    Skin.TalentTabTemplate(_G.PlayerTalentFrameTab1)
+    Skin.TalentTabTemplate(_G.PlayerTalentFrameTab2)
+    Skin.TalentTabTemplate(_G.PlayerTalentFrameTab3)
+    Skin.TalentTabTemplate(_G.PlayerTalentFrameTab4)
+    Skin.TalentTabTemplate(_G.PlayerTalentFrameTab5)
     Util.PositionRelative("TOPLEFT", bg, "BOTTOMLEFT", 20, -1, 1, "Right", {
-        _G.TalentFrameTab1,
-        _G.TalentFrameTab2,
-        _G.TalentFrameTab3,
-        _G.TalentFrameTab4,
-        _G.TalentFrameTab5,
+        _G.PlayerTalentFrameTab1,
+        _G.PlayerTalentFrameTab2,
+        _G.PlayerTalentFrameTab3,
+        _G.PlayerTalentFrameTab4,
+        _G.PlayerTalentFrameTab5,
     })
 
-    _G.TalentFrameScrollFrame:ClearAllPoints()
-    _G.TalentFrameScrollFrame:SetPoint("TOPLEFT", bg, 5, -45)
-    _G.TalentFrameScrollFrame:SetPoint("BOTTOMRIGHT", bg, -25, 30)
-    Skin.UIPanelScrollFrameTemplate(_G.TalentFrameScrollFrame)
-    local top, bottom = _G.TalentFrameScrollFrame:GetRegions()
+    _G.PlayerTalentFrameScrollFrame:ClearAllPoints()
+    _G.PlayerTalentFrameScrollFrame:SetPoint("TOPLEFT", bg, 5, -45)
+    _G.PlayerTalentFrameScrollFrame:SetPoint("BOTTOMRIGHT", bg, -25, 30)
+    Skin.UIPanelScrollFrameTemplate(_G.PlayerTalentFrameScrollFrame)
+    local top, bottom = _G.PlayerTalentFrameScrollFrame:GetRegions()
     top:Hide()
     bottom:Hide()
 
     for i = 1, _G.MAX_NUM_TALENTS do
-        Skin.TalentButtonTemplate(_G["TalentFrameTalent"..i])
+        Skin.TalentButtonTemplate(_G["PlayerTalentFrameTalent"..i])
     end
 end
